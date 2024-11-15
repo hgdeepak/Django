0b08e82bc6d746c64d367783ea1d9dc72141baa7
deepakhg <deepakhg@gmail.com>
2024-11-15 14:21:32 +0530

    Whitespace removal

--- mysitesrc/library/urls.py
+++ mysitesrc/library/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name='home'),
     path("books/", BookListView.as_view(), name="bookslist"),
     path("books/new/", BookCreateView.as_view(), name="newbook"),
-    path("books/<int:pk>", BookDetailView.as_view(), name="bookdetail"),   
+    path("books/<int:pk>", BookDetailView.as_view(), name="bookdetail"),
     path("books/edit/<int:pk>", BookUpdateView.as_view(), name="updatebook"),
     path("books/delete/<int:pk>", BookDeleteView.as_view(), name="deletebook"),
 ]
