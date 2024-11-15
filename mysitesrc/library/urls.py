625b330ee856bb783821684c5bbee36e3cf6f3b6
deepakhg <deepakhg@gmail.com>
2024-11-15 10:09:34 +0530

    Addition of Generic class based views

--- mysitesrc/library/urls.py
+++ mysitesrc/library/urls.py
@@ -3,10 +3,13 @@ URL module
 '''
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name='home'),
     path("books/", BookListView.as_view(), name="bookslist"),
-    path("books/<int:pk>", BookDetailView.as_view(), name="bookdetail"),
+    path("books/new/", BookCreateView.as_view(), name="newbook"),
+    path("books/<int:pk>", BookDetailView.as_view(), name="bookdetail"),    
+    path("books/edit/<int:pk>", BookUpdateView.as_view(), name="updatebook"),
+    path("books/delete/<int:pk>", BookDeleteView.as_view(), name="deletebook"),
 ]
--- mysitesrc/library/views.py
+++ mysitesrc/library/views.py
@@ -2,6 +2,8 @@
 Library View Module
 '''
 from django.views.generic import ListView, DetailView
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Book
 
 # Create your views here.
@@ -20,3 +22,33 @@ class BookDetailView(DetailView):
     model = Book
     template_name = "BookDetail.html"
     context_object_name = "book"
+
+class BookCreateView(CreateView):
+    '''
+    Generic class-based view to create new book data
+    '''
+    model = Book
+    template_name = "NewBook.html"
+    fields = ("title", "author", "isbn", "description", "price")
+
+    def get_success_url(self):
+        return reverse_lazy("bookslist", kwargs={"pk": self.object.id})
+
+
+class BookUpdateView(UpdateView):
+    '''
+    Generic class-based view to create new book data
+    '''
+    model = Book
+    template_name = "NewBook.html"
+    fields = ("title", "author", "isbn", "description", "price")
+
+    def get_success_url(self):
+        return reverse_lazy("bookdetail", kwargs={"pk": self.object.id})
+class BookDeleteView(DeleteView):
+    '''
+    Book Delete View
+    '''
+    model = Book
+    template_name = "Book_confirm_delete.html"
+    success_url = reverse_lazy("bookslist")
